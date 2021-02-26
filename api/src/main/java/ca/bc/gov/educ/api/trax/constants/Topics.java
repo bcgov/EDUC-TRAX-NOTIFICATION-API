858fc681c7186d256daa49bd19da25833794f4d0
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-02-25 20:46:27 -0800

    fixing topics interested...

--- api/src/main/java/ca/bc/gov/educ/api/trax/constants/Topics.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/constants/Topics.java
@@ -5,14 +5,6 @@ package ca.bc.gov.educ.api.trax.constants;
  */
 public enum Topics {
   /**
-   * Student events topic topics.
-   */
-  STUDENT_EVENTS_TOPIC,
-  /**
-   * Pen match events topic topics.
-   */
-  PEN_MATCH_EVENTS_TOPIC,
-  /**
    * Pen services events topic topics.
    */
   PEN_SERVICES_EVENTS_TOPIC
