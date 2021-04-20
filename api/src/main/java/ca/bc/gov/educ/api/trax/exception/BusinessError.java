c5f08feda28825a834cdb2aedbab0d4e888b97ab
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-04-20 14:47:16 -0700

    code changes to move from STAN to JetStream

--- api/src/main/java/ca/bc/gov/educ/api/trax/exception/BusinessError.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/exception/BusinessError.java
@@ -9,7 +9,7 @@ public enum BusinessError {
   /**
    * The Event already persisted.
    */
-  EVENT_ALREADY_PERSISTED("Event with event id :: $? , is already persisted in DB, a duplicate message from STAN.");
+  EVENT_ALREADY_PERSISTED("Event with event id :: $? , is already persisted in DB, a duplicate message from Jet Stream.");
 
   @Getter
   private final String code;
